529cec0da24ec6dd29a33366ee0436e6d67e9e14
David <dpape39@icloud.com>
2019-02-22 12:34:42 +0100

    first layer now only serves to take inputs

--- src/NeuralNet/Layer.java
+++ src/NeuralNet/Layer.java
@@ -1,6 +1,7 @@
 package NeuralNet;
 
 import ActivationFunctions.ActivationFunction;
+import ActivationFunctions.Identity;
 import org.ejml.data.DMatrixRMaj;
 import org.ejml.dense.row.CommonOps_DDRM;
 
@@ -8,20 +9,38 @@ import java.util.function.DoubleSupplier;
 
 public class Layer {
     private ActivationFunction function;
-    private DMatrixRMaj weights;
+    private DMatrixRMaj inputWeights;
 
+    /**
+     * create input layer: neuron i passes input i on to next layer unmodified
+     * @param inputSize
+     */
+    public Layer(int inputSize) {
+        inputWeights = new DMatrixRMaj(inputSize, inputSize);
+        this.function = new Identity();
+        for (int i = 0; i < inputSize; i++)
+            inputWeights.set(i, i, 1);
+    }
+
+    /**
+     * create ordinary layer with weights given by supplier
+     * @param size
+     * @param inputSize
+     * @param function
+     * @param weightSupplier
+     */
     public Layer(int size, int inputSize, ActivationFunction function, DoubleSupplier weightSupplier) {
-        weights = new DMatrixRMaj(inputSize, size);
+        inputWeights = new DMatrixRMaj(inputSize, size);
         this.function = function;
 
-        for (int i = 0; i < weights.getNumRows(); i++)
-            for (int j = 0; j < weights.getNumCols(); j++)
-                weights.set(i, j, weightSupplier.getAsDouble());
+        for (int i = 0; i < inputWeights.getNumRows(); i++)
+            for (int j = 0; j < inputWeights.getNumCols(); j++)
+                inputWeights.set(i, j, weightSupplier.getAsDouble());
     }
 
     public DMatrixRMaj think(DMatrixRMaj input) {
         DMatrixRMaj result = new DMatrixRMaj(input.getNumRows(), 1);
-        CommonOps_DDRM.mult(weights, input, result);
+        CommonOps_DDRM.mult(inputWeights, input, result);
         for (int i = 0; i < result.getNumRows(); i++)
             result.set(i, 0, function.apply(result.get(i, 0)));
         return result;
@@ -36,6 +55,6 @@ public class Layer {
     }
 
     public int getSize() {
-        return weights.getNumCols();
+        return inputWeights.getNumCols();
     }
 }
--- src/NeuralNet/NeuralNetwork.java
+++ src/NeuralNet/NeuralNetwork.java
@@ -15,18 +15,20 @@ public class NeuralNetwork {
 
     /**
      * Create a new NN. Input size is size of first layer, output size is size of last layer
-     * @param layerSizes Gives number of layers and the size of each layer
+     * Input layer (first layer) passes inputs on to hidden layers unmodified. Only exists to fix input size
+     * Output layer (last layer) can have a custom activation function and weights
+     * @param layerSizes Gives number of layers and the size of each layer including input and output layers
      * @param functions functions[i] = activation function of i-th layer
      * @throws IllegalArgumentException if lengths of layer and function arrays don't match
      */
     public NeuralNetwork(int[] layerSizes, ActivationFunction[] functions, DoubleSupplier weightSupplier) throws IllegalArgumentException {
         if(layerSizes.length != functions.length)
             throw new IllegalArgumentException("cannot construct net: number of layers and functions do not match");
-        if(layerSizes.length < 1)
-            throw new IllegalArgumentException("cannot construct net: number of layers is zero");
+        if(layerSizes.length < 2)
+            throw new IllegalArgumentException("cannot construct net: number of hidden layers is zero");
 
         layers = new ArrayList<>(layerSizes.length);
-        layers.add(new Layer(layerSizes[0], layerSizes[0], functions[0], weightSupplier));
+        layers.add(new Layer(layerSizes[0]));
         for (int i = 1; i < layerSizes.length; i++)
             layers.add(new Layer(layerSizes[i], layerSizes[i - 1], functions[i], weightSupplier));
     }
